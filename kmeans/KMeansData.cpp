b5129e8aa3cf7218e6da04e6ab13b12d7512c1b1
klorel <manuel.jean.ruiz@gmail.com>
2012-08-10 09:59:44 +0200

    kmeans ++

--- kmeans/KMeansData.cpp
+++ kmeans/KMeansData.cpp
@@ -6,9 +6,9 @@
  */
 
 #include "KMeansData.hpp"
-
+#include "IndexedList.hpp"
 KMeansData::KMeansData(size_t const & n, size_t const & m, size_t const & k) :
-		RawData(n, m), _k(k) {
+		RawData(n, m), _clusters(k) {
 	buildDiag();
 }
 
@@ -22,53 +22,109 @@ void KMeansData::buildDiag() {
 }
 void KMeansData::init(IntVector const & x) {
 	_x = x;
-	_sumK.assign(_k, 0);
-	_sumW.assign(_k, 0);
-	_list.assign(_k, IntList());
+	_clusters.assign(_clusters.size(), ClusterData());
 	for (size_t i(0); i < getN(); ++i) {
 		size_t const c(_x[i]);
-		_sumK[c] += _kii[i] * _weights[i];
-		_sumW[c] += _weights[i];
-		_list[c].push_front(i);
-		_position[i] = _list.begin();
+		_clusters[c].weight += _weights[i];
+		_clusters[c].list.push_front(i);
+		_position[i] = _clusters[c].list.begin();
 	}
 }
 void KMeansData::setWeights(DoubleVector const & weights) {
 	_weights = weights;
 }
-bool KMeansData::loop() {
-	return true;
-}
 size_t KMeansData::getK() const {
-	return _k;
-}
-size_t KMeansData::loop(size_t const & i) {
-	_buffer.resize(_k);
-	return 0;
+	return _clusters.size();
 }
 
-double KMeansData::d(size_t const & i, size_t const & c) const {
+double KMeansData::getIntra(size_t const & c) const {
+	double result(0);
+	for (auto const & i : _clusters[c].list) {
+		for (auto const & j : _clusters[c].list) {
+			if (i < j) {
+				result += 2 * _weights[i] * _weights[j] * get(i, j);
+			}
+		}
+	}
+	return result / (_clusters[c].weight * _clusters[c].weight);
+}
+void KMeansData::getIntra() {
+	for (size_t k(0); k < getK(); ++k)
+		_clusters[k].intra = getIntra(k);
+}
+double KMeansData::getLinearPart(size_t const & i, size_t const & c) const {
+	double result(0);
+	for (auto const & j : _clusters[c].list) {
+		result -= (2 * _weights[j] * get(i, j));
+	}
+	return result / _clusters[c].weight;
+
+}
+double KMeansData::getD(size_t const & i, size_t const & c) const {
 	double result(0);
 	result += _kii[i];
-	result -= (2 * _sumK[c] / _sumW[c]);
+	result += getLinearPart(i, c);
+	result += getIntra(c);
 
 	return result;
 }
 void KMeansData::initLoop() {
-	_buffer.assign(getK(), 0);
-	for (size_t k(0); k < getK(); ++k) {
-
-	}
+	getIntra();
+//	_buffer.assign(getK(), 0);
+//	for (size_t k(0); k < getK(); ++k) {
+//
+//	}
 }
 void KMeansData::move(size_t const & i, size_t const & c) {
 	if (_x[i] != c) {
-		_sumW[_x[i]] -= _weights[i];
-		_list[_x[i]].erase(_position[i]);
+		_clusters[_x[i]].weight -= _weights[i];
+		_clusters[_x[i]].list.erase(_position[i]);
 
-		_sumW[c] += _weights[i];
-		_list[c].push_back(i);
+		_clusters[c].weight += _weights[i];
+		_clusters[c].list.push_back(i);
 
-		_position[i] = _list[c].begin();
+		_position[i] = _clusters[c].list.begin();
 		_x[i] = c;
 	}
+}
+
+size_t KMeansData::getClosest(size_t const & i) {
+	size_t bestC(getK());
+	double minD(std::numeric_limits<double>::max());
+	for (size_t c(0); c < getK(); ++c) {
+		double const d(getD(i, c));
+		if (d < minD) {
+			bestC = c;
+			minD = d;
+		}
+	}
+	return bestC;
+}
+bool KMeansData::loop(size_t const & maxIte) {
+	IndexedList all(getK(), true);
+	return loop(all, maxIte);
+
+}
+bool KMeansData::loop(IndexedList const & sub, size_t const & maxIte) {
+	bool improvement(false);
+	std::vector<std::pair<size_t, size_t> > moves;
+	moves.reserve(getN());
+	size_t ite(0);
+	do {
+		++ite;
+		moves.clear();
+		initLoop();
+		for (auto const & c : sub) {
+			for (auto const & i : _clusters[c].list) {
+				size_t const newC(getClosest(i));
+				if (newC != _x[i])
+					moves.push_back(std::make_pair(i, newC));
+			}
+		}
+		for (auto const & m : moves)
+			move(m.first, m.second);
+		if (!moves.empty())
+			improvement = true;
+	} while (!moves.empty() && ite < maxIte);
+	return improvement;
 }
--- kmeans/KMeansData.hpp
+++ kmeans/KMeansData.hpp
@@ -10,35 +10,47 @@
 
 #include "RawData.hpp"
 
+struct ClusterData {
+	//
+	double weight;
+	//
+	double intra;
+	//
+	IntList list;
+
+	ClusterData() :
+			weight(0), intra(0), list() {
+	}
+};
+
+typedef std::vector<ClusterData> ClusterDatas;
+
 class KMeansData: public RawData {
 public:
 	KMeansData(size_t const & n, size_t const & m, size_t const & k);
 	virtual ~KMeansData();
 public:
-	bool loop();
-	size_t loop(size_t const & i);
+	bool loop(size_t const & maxIte = 0);
+	bool loop(IndexedList const & sub,size_t const & maxIte = 0);
+	size_t getClosest(size_t const & i);
 	size_t getK() const;
 	void setWeights(DoubleVector const &);
 	void init(IntVector const &);
 private:
-	double d(size_t const & i, size_t const & c) const;
+	double getD(size_t const & i, size_t const & c) const;
 	void move(size_t const & i, size_t const & c);
 	void initLoop();
+	void getIntra();
+	double getLinearPart(size_t const & i, size_t const & c) const;
+	double getIntra(size_t const & c) const;
 private:
 	void buildDiag();
-	size_t _k;
+
 	IntVector _x;
 	DoubleVector _weights;
 
 	DoubleVector _kii;
-
-	DoubleVector _sumK;
-	DoubleVector _sumW;
-	DoubleVector _intra;
-
-	DoubleVector _buffer;
-
-	IntListVector _list;
+	ClusterDatas _clusters;
 	std::vector<IntList::iterator> _position;
 };
 
