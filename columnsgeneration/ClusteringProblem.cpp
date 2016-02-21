da604704065f813c1e7a8e3338775bcbc44cacaf
klorel <manuel.jean.ruiz@gmail.com>
2016-02-21 17:00:21 +0100

    --

---
+++ columnsgeneration/ClusteringProblem.cpp
@@ -0,0 +1,63 @@
+#include "ClusteringProblem.h"
+#include "Node.h"
+
+ClusteringProblem::ClusteringProblem() {
+	_exactOracle = NULL;
+	_vnsOracle = NULL;
+}
+ClusteringProblem::~ClusteringProblem() {
+
+}
+
+void ClusteringProblem::setExactOracle(IOracle * oracle) {
+	_exactOracle = oracle;
+}
+void ClusteringProblem::setVnsOracle(IOracle *oracle) {
+	_vnsOracle = oracle;
+}
+IOracle * ClusteringProblem::getExactOracle() const {
+	return _exactOracle;
+}
+IOracle * ClusteringProblem::getVnsOracle() const {
+	return _vnsOracle;
+}
+
+void ClusteringProblem::branchingSelection(Node const & node, size_t &noeud1,
+		size_t &noeud2) const {
+	DecisionSet decisions;
+	node.decisions(decisions);
+	BranchingWeights weights;
+	branchingWeights(node.lbSolution(), weights);
+	std::pair<size_t, size_t> const noeud1noeud2(
+			branchingSelection(decisions, weights));
+	noeud1 = noeud1noeud2.first;
+	noeud2 = noeud1noeud2.second;
+}
+
+std::pair<size_t, size_t> ClusteringProblem::branchingSelection(
+		DecisionSet const & decisions, BranchingWeights & weights) const {
+	BranchingWeights::const_iterator it(weights.begin());
+	while (decisions.find(
+			Decision(std::make_pair(it->second.first, it->second.second)))
+			!= decisions.end() && it != weights.end()) {
+		++it;
+	}
+	return it->second;
+}
+
+bool ClusteringProblem::checkGradient(IndexedList const & nodes,
+		DoubleVector const & g) const {
+	bool result(true);
+	DoubleVector values(nV(), 0);
+	gradient(nodes, values);
+	for (size_t v(0); v < nV(); ++v) {
+		if (std::fabs(g[v] - values[v]) > 1e-10) {
+			result = false;
+			std::cout << "WRONG GRADIENT " << std::setw(6) << v;
+			std::cout << std::setw(16) << g[v];
+			std::cout << std::setw(16) << values[v];
+			std::cout << std::endl;
+		}
+	}
+	return result;
+}
---
+++ columnsgeneration/ClusteringProblem.h
@@ -0,0 +1,54 @@
+/*
+ * ClusteringProblem.h
+ *
+ *  Created on: 12 mars 2014
+ *      Author: manuel
+ */
+
+#ifndef I_CLUSTERING_PROBLEM_H_
+#define I_CLUSTERING_PROBLEM_H_
+
+#include "common.h"
+#include "Edge.h"
+#include "gencol.h"
+class IOracle;
+class Node;
+
+typedef std::vector<std::map<size_t, Double>> AdjencyGraph;
+
+class ClusteringProblem {
+public:
+	virtual size_t nV() const =0;
+	virtual void writeSolution(FractionnarySolution const&, double) const = 0;
+	virtual void update(size_t id, bool wasIn, DoubleVector &gradient) const= 0;
+	virtual void gradient(IndexedList const & v, DoubleVector &) const= 0;
+	virtual Double computeCost(std::set<size_t> const &) const = 0;
+	virtual Double computeCost(IndexedList const &) const = 0;
+	virtual void branchingWeights(FractionnarySolution const &,
+			BranchingWeights & result) const = 0;
+public:
+	virtual void branchingSelection(Node const & node, size_t &noeud1,
+			size_t &noeud2) const;
+
+	virtual std::pair<size_t, size_t> branchingSelection(
+			DecisionSet const & decisions, BranchingWeights & weights) const;
+
+	virtual bool checkGradient(IndexedList const & v,
+			DoubleVector const & g) const;
+
+	virtual IOracle * getExactOracle() const;
+
+	virtual IOracle * getVnsOracle() const;
+
+public:
+	void setExactOracle(IOracle *);
+	void setVnsOracle(IOracle *);
+public:
+	ClusteringProblem();
+	virtual ~ClusteringProblem();
+private:
+	IOracle * _exactOracle;
+	IOracle * _vnsOracle;
+};
+
+#endif /* ICLIQUEPARTITIONNINPROBLEM_H_ */
